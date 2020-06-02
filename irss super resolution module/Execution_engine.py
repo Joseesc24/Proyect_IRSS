adffeb8c92fc9b2e0f961cb37aa4bf472f9557ed
Joseesc24 <josed.escobar@outlook.com>
2020-06-01 23:46:44 -0500

    integracion con el front

--- irss super resolution module/Execution_engine.py
+++ irss super resolution module/Execution_engine.py
@@ -39,8 +39,8 @@ def download_image_from_s3(image_name, bucket_name):
     boto3.client('s3').download_fileobj(bucket_name, image_name, open('./LR/' + image_name, 'wb'))
 
 
-def lambda_upload_file_to_s3(image_name, bucket_name):
-    boto3.client('s3').upload_fileobj(open('/HR/' + image_name, 'rb'), bucket_name, image_name)
+def lambda_upload_file_to_s3(image_name, bucket_name, new_image_name):
+    boto3.client('s3').upload_fileobj(open('./HR/' + image_name, 'rb'), bucket_name, new_image_name)
 
 
 def delete_file_from_s3(filename, bucket_name):
@@ -48,28 +48,33 @@ def delete_file_from_s3(filename, bucket_name):
 
 
 while True:
-    print(queue.receive_messages())
-    for message in queue.receive_messages():
-        codigo_imagen = message.body
-        print("Codigo de la siguiente imagen:" + str(codigo_imagen))
-        if check_if_item_exist_dynamo(codigo_imagen) == True:
-            estado = get_item_dynamodb(codigo_imagen)
-            nombre_imagen = str(estado['image_original_name'])
-            paso_imagen = int(estado['image_estate'])
-            print("Estado de la imagen: " + str(paso_imagen))
-            print("Nombre de la imagen: " + str(nombre_imagen))
-            if paso_imagen == 1:
-                download_image_from_s3(nombre_imagen, download_bucket)
-                run_esrgan()
-                if os.path.exists('./LR/' + nombre_imagen):
-                    os.remove('./LR/' + nombre_imagen)
-                nombre_imagen, extension = nombre_imagen.split(".")
-                nombre_imagen = nombre_imagen + "_rlt." + extension
-                lambda_upload_file_to_s3(nombre_imagen, upload_bucket)
-                if os.path.exists('./HR/' + nombre_imagen):
-                    os.remove('./HR/' + nombre_imagen)
-                delete_file_from_s3(nombre_imagen, download_bucket)
-                print("Imagen " + nombre_imagen + " escalada y entrgada en " + upload_bucket)
+
+    for message in queue.receive_messages(MessageAttributeNames=['All']):
+        print("")
+        nombre_imagen = str(message.message_attributes['image_name']['StringValue'])
+        hash_imagen = str(message.message_attributes['image_hash']['StringValue'])
+        print("Nombre de la siguiente imagen: " + str(nombre_imagen))
+        print("Hash de la siguiente imagen: " + hash_imagen)
+        download_image_from_s3(nombre_imagen, download_bucket)
+        print("Imagen descargada")
+        run_esrgan()
+        print("Imagen reescalada")
+        if os.path.exists('./LR/' + nombre_imagen):
+            os.remove('./LR/' + nombre_imagen)
+            print("Imagen original borrada del almacenamiento local")
+        nombre_imagen, extension = nombre_imagen.split(".")
+        nombre_imagen = nombre_imagen + "_rlt." + extension
+        print("Nombre de la imagen reescalada: " + nombre_imagen)
+        time.sleep(4)
+        new_image_name = hash_imagen + extension
+        lambda_upload_file_to_s3(nombre_imagen, upload_bucket, new_image_name)
+        print("Imagen reescalda subida a s3")
+        if os.path.exists('./HR/' + nombre_imagen):
+            os.remove('./HR/' + nombre_imagen)
+            print("Imagen reescalada borrada del almacenamiento local")
+        delete_file_from_s3(nombre_imagen, download_bucket)
+        print("Imagen original borrada de s3")
+        print("Imagen " + nombre_imagen + " escalada y entrgada en " + upload_bucket)
 
         message.delete()
         time.sleep(60)
